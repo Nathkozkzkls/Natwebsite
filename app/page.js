b1c1704f369e0f73202726a8b9f3218b87acef91
Nathkozkzkls <167110605+Nathkozkzkls@users.noreply.github.com>
2024-06-22 22:25:27 +0200

    page.js

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     <a className="inline-block px4 py-2 rounded-lg bg-[#8800d4] mt-8"href="https://discord.com/oauth2/authorize?client_id=1230107519406309417&permissions=8&integration_type=0&scope=bot+applications.commands">Ajouter / • Asteroid Bot</a>
     <Image className="mt-96 mx-auto" src="/images/IMG_0930.webp" alt="description" width={512} height={512}/>
     <h2 className="mt-0 text-xl">Le robot dont votre serveur a besoin.</h2>
-    <p className="text-sm">Asteroid Bot est le bot dont votre serveur à besoin, Asteroid Bot est un bot multifonctions polyvalent, prêt à tout pour être ajouté.</p>
+    <p className="text-sm">Asteroid Bot est le bot dont votre serveur a besoin, Asteroid Bot est un bot multifonctions polyvalent, prêt à tout pour être ajouté.</p>
     </div>
     </div>
     </div>
