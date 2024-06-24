4d26b778b66adff2974bcf50fe1c5d39c8710d63
Nathkozkzkls <167110605+Nathkozkzkls@users.noreply.github.com>
2024-06-24 18:34:53 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+qu/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
