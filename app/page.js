addee346c895ad1eb31ac56c06438524d50131b0
Nathkozkzkls <167110605+Nathkozkzkls@users.noreply.github.com>
2024-07-22 18:04:34 +0200

    page.js

--- app/page.js
+++ app/page.js
@@ -8,12 +8,17 @@ export default function Home() {
     <div className="mt-8">
     <div className="mt-96">
     <div className="mt-0">
+    <div className="text-lg">
     <h1 className="text-5xl my-6 text-[#8800d4]">Asteroid Bot</h1>
     <p className="text-xl -mx-auto">Le robot multifonctions</p>
     <a className="inline-block px4 py-2 rounded-lg bg-[#8800d4] mt-8"href="https://discord.com/oauth2/authorize?client_id=1230107519406309417&permissions=8&integration_type=0&scope=bot+applications.commands">Ajouter / • Asteroid Bot</a>
     <Image className="mt-96 mx-auto" src="/images/IMG_0930.webp" alt="description" width={512} height={512}/>
     <h2 className="mt-0 text-xl">Le robot dont votre serveur a besoin.</h2>
     <p className="text-sm">Asteroid Bot est le bot dont votre serveur a besoin, Asteroid Bot est un bot multifonctions polyvalent, prêt à tout pour être ajouté.</p>
+    <Image className="mt-96 mx-auto" src="/images/IMG_1117.webp" alt="description" width={512} height={512}/>
+    <h3 className="mt-0 text-xl">AutoMod</h3>
+    <p className="text-sm">Protégez vos serveurs dès maintenant avec Asteroid Bot et son AutoMod !</p>
+    </div>
     </div>
     </div>
     </div>
